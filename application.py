cb8935d6ae909107ee07c918a02d8486503644be
Anna Popovychenko <a.popovychenko@gmail.com>
2019-12-03 17:38:44 +0200

    Added oidc

--- application.py
+++ application.py
@@ -1,9 +1,47 @@
 from flask import Flask
+from flask_oidc import OpenIDConnect
 app = Flask(__name__)
+oidc = OpenIDConnect(app)
+
+THUMBPRINT = '698BCD64688E433EAEB3717CB13B011F5D2E573C'
+
 
 @app.route("/")
 def hello():
+    with open(f'/var/ssl/certs/{THUMBPRINT}.der') as f:
+        pass
     return "Hello World!"
 
 
+# @app.route("/.well-known/openid-configuration")
+# def keys():
+    # return Content(JsonConvert.SerializeObject(new OidcModel
+    #             {
+    #                 // Sample: The issuer name is the application root path
+    #                 Issuer = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase.Value}/",
+    issuer = "https://invite-app.azurewebsites.net/"
+    #
+    #                 // Sample: Include the absolute URL to JWKs endpoint
+    #                 JwksUri = Url.Link("JWKS", null),
+    #
+    #                 // Sample: Include the supported signing algorithms
+    #                 IdTokenSigningAlgValuesSupported = new[] { OidcController.SigningCredentials.Value.Algorithm},
+    #             }), "application/json");
+
+    # https://flask-oidc.readthedocs.io/en/latest/
+    # with open(f'/var/ssl/certs/{THUMBPRINT}.der') as f:
+    #     pass
+    # return {}
+
+
+# @app.route("/.well-known/keys", name="JWKS")
+# def keys2():
+#     # return Content(JsonConvert.SerializeObject(new JwksModel
+#     #             {
+#     #                 Keys = new[] { JwksKeyModel.FromSigningCredentials(OidcController.SigningCredentials.Value) }
+#     #             }), "application/json");
+#     pass
+
+
 # az webapp up --sku FREE -n "inviteapp2" -l "Central US"
+# az webapp config appsettings set --name invite-app --resource-group ringplan --settings WEBSITE_LOAD_CERTIFICATES=*
